d261dcc9ba087adb9d4fff44432815b25fd70d7a
Max Wofford <max@maxwofford.com>
2015-12-18 03:24:58 -0800

    Fix unset twilio_number warnings

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser());
 const port = process.env.PORT || 3000;
 const account_sid = process.env.TWILIO_ACCOUNT_SID || console.log('WARN: SID not set!');
 const auth_token = process.env.TWILIO_AUTH_TOKEN || console.log('WARN: TOKEN not set!');
-const twilio_number = process.env.TWILIO_NUMBER || console.log('WARN: TOKEN not set!');
+const twilio_number = process.env.TWILIO_NUMBER || console.log('WARN: TWILIO_NUMBER not set!');
 const client = require('twilio')(account_sid, auth_token);
 const secretMessage = process.env.SECRET_MESSAGE || 'babydinosaur';
 
