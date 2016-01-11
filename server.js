7f213d4b45ab74c8deaba3ef3b44ef818b653238
Max Wofford <max@maxwofford.com>
2016-01-11 01:03:45 -0800

    Comment individual parts of server.js

--- server.js
+++ server.js
@@ -15,6 +15,9 @@ const client = require('twilio')(account_sid, auth_token);
 const secretMessage = process.env.SECRET_MESSAGE || 'babydinosaur';
 
 function sendSms(phone_number, message) {
+  // This is all from the twilio-node package
+  // https://twilio.github.io/twilio-node/
+
   client.sendMessage({
     to: phone_number,
     from: twilio_number,
@@ -29,15 +32,25 @@ function sendSms(phone_number, message) {
 let phone_numbers = [];
 
 app.use(function *() {
-  phone_numbers.push(this.request.body.From);
+  // Store the phone number of anyone who sends us an SMS
+  let sender = this.request.body.From;
+  console.log(`Recieved SMS from ${sender}`);
+  phone_numbers.push(sender);
+
+  // If this person has sent the secret message, trigger a mass response
   if (this.request.body.Body === secretMessage) {
+    console.log(`Recieved secret message from ${sender}`);
+    // Make sure we don't have any duplicate numbers stored
     let phone_number_set = new Set();
     phone_numbers.forEach((item) => {
       phone_number_set.add(item);
     });
+    // Send a message to everyone in our phone_number_set
     for (let phone_number of phone_number_set) {
+      console.log(`Sending SMS to ${phone_number}`);
       sendSms(phone_number, 'This message is sent with code!');
     }
+    // Drop our contacts list
     phone_numbers = [];
   }
   this.body = 'success';
